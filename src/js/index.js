41dfa3d87af0c4bd13187365a22c91cdff60e542
Daniel Ipince <ipince22@gmail.com>
2020-02-06 05:56:42 +0000

    react practice

--- src/js/index.js
+++ src/js/index.js
@@ -9,7 +9,8 @@ import "bootstrap";
 import "../styles/index.scss";
 
 //import your own components
-import { Home } from "./component/home.js";
+//import { Home } from "./component/home.js";
+import TrafficLight from "./component/TrafficLight.js";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+ReactDOM.render(<TrafficLight />, document.querySelector("#app"));
